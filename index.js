5731e9ee9edbc5193fee793e0b38403dd704568c
Denis Hedgehog <ezhoden@mail.ru>
2018-05-11 18:15:42 +0300

    some fixex #19

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.post('/auth', function (req, res) {
     db.knex('users').where('login', req.body.login).select().then(result => {
         if (result.length > 0) {
             console.log(result)
-            console.log("RESULT PASSWORD" + result.password)
+            console.log("RESULT PASSWORD" + result[0].password)
             console.log("USER PASSWORD" + req.body.password)
             console.log(result.password + " === " + req.body.password + " IS " + result.password === req.body.password)
             if (result.password === req.body.password) {
